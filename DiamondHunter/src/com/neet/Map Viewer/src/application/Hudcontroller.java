27afbf9f6d083ef86b64e412077b011aca496d76
Lullahan <45283727+Lullahan@users.noreply.github.com>
2018-12-11 20:36:45 +0800

    Added Cursor Controls

--- DiamondHunter/src/com/neet/Map Viewer/src/application/Hudcontroller.java
+++ DiamondHunter/src/com/neet/Map Viewer/src/application/Hudcontroller.java
@@ -1,15 +1,27 @@
 package application;
-import java.net.URL;
-import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class Hudcontroller {
 
-    @FXML
-    private ResourceBundle resources;
+	@FXML
+	private void handleKeyAction(KeyEvent event) {
 
-    @FXML
-    private URL location;
+	    if (event.getCode() == KeyCode.W || event.getCode() == KeyCode.UP) {
+	    	Main.mapDisplay.cursorUp();
+	    }
+	    else if (event.getCode() == KeyCode.S || event.getCode() == KeyCode.DOWN) {
+	    	Main.mapDisplay.cursorDown();
+	    }
+	    else if (event.getCode() == KeyCode.A || event.getCode() == KeyCode.LEFT) {
+	    	Main.mapDisplay.cursorLeft();
+	    }
+	    else if (event.getCode() == KeyCode.D || event.getCode() == KeyCode.RIGHT) {
+	    	Main.mapDisplay.cursorRight();
+	    }
+	}
 
     @FXML
     void initialize() {
