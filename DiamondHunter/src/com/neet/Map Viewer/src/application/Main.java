daadf4d8c58dfccc2d80e9181ded09edc65bf4f1
Lullahan <45283727+Lullahan@users.noreply.github.com>
2018-12-11 17:58:08 +0800

    Map Display Working

--- DiamondHunter/src/com/neet/Map Viewer/src/application/Main.java
+++ DiamondHunter/src/com/neet/Map Viewer/src/application/Main.java
@@ -1,13 +1,20 @@
 package application;
-	
+
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.TilePane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	public static Stage primaryStage;
+	public static MapDisplay mapDisplay;
+	
+	public BorderPane root;
+	public TilePane tile;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -15,16 +22,51 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage){
-		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Hud.fxml"));
-			Scene scene = new Scene(root,800,500);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Map Viewer");
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+		Main.primaryStage = primaryStage;
+        Main.primaryStage.setTitle("MapViewer");
+
+        initialise();
+        
+		showMap();
 		}
-	}
+	
+	public void showMap() {
+		mapDisplay = new MapDisplay();
+		mapDisplay.loadMapFile("./testmap.map");
+		mapDisplay.loadImagesFiles("./testtileset.gif", "./items.gif");
+		
+			try {
+				FXMLLoader loader = new FXMLLoader();
+				loader.setLocation(Main.class.getResource("test.fxml"));
+				
+				tile = (TilePane) loader.load();
+			} catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			
+		tile.setPrefColumns(mapDisplay.numCols);
+		tile.setPrefRows(mapDisplay.numRows);
+		mapDisplay.initialiseCanvas();
+		tile.getChildren().add(mapDisplay.currentCanvas);
+		
+		root.setCenter(tile);
+		
+		}
+	public void initialise() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("Hud.fxml"));
+            root = (BorderPane) loader.load();
+
+            Scene scene = new Scene(root);
+            
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+}
 }
 	
--- DiamondHunter/src/com/neet/Map Viewer/src/application/MapDisplay.java
+++ DiamondHunter/src/com/neet/Map Viewer/src/application/MapDisplay.java
@@ -1,43 +1,83 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
-public class MapDisplay {
-	
-	private int tileSize;
-	int numCols;
-	int numRows;
-	private int[][] map;
-	int numTilesAcross;
-	Image tileset;
-	Image items;
-	private Image mapImage;
+public class MapDisplay implements getPositions{
+	private int boatRow = -1;
+	private int boatCol = -1;
+	private int axeRow = -1;
+	private int axeCol = -1;
 	
 	public MapDisplay() {
-		tileSize = 16;
-		new TileMap(tileSize);
-		loadMapFile("testmap.map");
-		loadImagesFiles("testtileset.gif","items.gif");
-		initialiseCanvas();
 		
 	}
 	
+	public int getBoatRow() {
+		return boatRow;
+	}
+	public int getBoatCol() {
+		return boatCol;
+	}
+	public int getAxeRow() {
+		return axeRow;
+	}
+	public int getAxeCol() {
+		return axeCol;
+	}
+	
+	private final int BOAT = 0;
+	private final int AXE = 1;
+
+	private int tileSize = 16;
+	public int numCols;
+	public int numRows;
+	
+	public MapCursor cursor;
+	public boolean cursorColor = false;
+
+	private int currentNumCols;
+	private int currentNumRows;
+
+	private int[][] mapMatrix;
+	private int[][] tileType;
+	
+	private Image tileset;
+	private int numTilesAcross;
+
+	private Canvas mainCanvas;
+	public Canvas currentCanvas;
+	private Image mapImage;
+	private Image originalMapImage;
+
+
+	public int movesetCols;
+	public int movesetRows;
+
+	public Image items;
+	public boolean axePut = false;
+	public boolean boatPut = false;
 	
 	public void loadMapFile(String mapFile) {
 		try {
-			java.io.InputStream in = getClass().getResourceAsStream(mapFile);
+			InputStream in = getClass().getResourceAsStream(mapFile);
+			if (in == null) {
+				System.out.print("X");
+			} else {
+				System.out.println("Y");
+			}
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
 			numCols = Integer.parseInt(br.readLine());
 			numRows = Integer.parseInt(br.readLine());
+			currentNumCols = numCols;
+			currentNumRows = numRows;
 
-			int[][] mapMatrix = new int[numRows][numCols];
-
+			mapMatrix = new int[numRows][numCols];
 			String delims = "\\s+";
 			for(int row = 0; row < numRows; row++) {
 				String line = br.readLine();
@@ -63,15 +103,18 @@ public class MapDisplay {
 		}
 	}
 
+
 	public void initialiseCanvas() {
-		Canvas mainCanvas = new Canvas(640,640);
-		int[][] tileType = new int[numRows][numCols];
+		mainCanvas = new Canvas(640,640);
+		currentCanvas = new Canvas(640, 640);
+		tileType = new int[numRows][numCols];
+		cursor = new MapCursor();
 
 		for(int row = 0; row < numRows; row++) {
 			for(int col = 0; col < numCols; col++) {
-				if(map[row][col] == 0) continue;
+				if(mapMatrix[row][col] == 0) continue;
 
-				int rc = map[row][col];
+				int rc = mapMatrix[row][col];
 
 				int r = rc / numTilesAcross;
 				int c = rc % numTilesAcross;
@@ -80,9 +123,16 @@ public class MapDisplay {
 					mainCanvas.getGraphicsContext2D().drawImage(
 							tileset, c * tileSize, 0, tileSize, tileSize,
 							col * tileSize, row * tileSize, tileSize, tileSize);
+					currentCanvas.getGraphicsContext2D().drawImage(
+							tileset, c * tileSize, 0, tileSize, tileSize,
+							col * tileSize, row * tileSize, tileSize, tileSize);
+					tileType[row][col] = 0;
 				}
 				else {
 					mainCanvas.getGraphicsContext2D().drawImage(
+							tileset, c * tileSize, tileSize, tileSize, tileSize,
+							col * tileSize, row * tileSize, tileSize, tileSize);
+					currentCanvas.getGraphicsContext2D().drawImage(
 							tileset, c * tileSize, tileSize, tileSize, tileSize,
 							col * tileSize, row * tileSize, tileSize, tileSize);
 					tileType[row][col] = 1;
@@ -90,7 +140,242 @@ public class MapDisplay {
 
 			}
 		}
+		originalMapImage = mainCanvas.snapshot(null, null);
+		drawCursorToMainCanvas();
+		currentCanvas.getGraphicsContext2D().drawImage(
+				cursor.imageOption[cursor.current], 0, 0, tileSize, tileSize,
+				 cursor.cursorCols * tileSize, cursor.cursorRows * tileSize,
+				 tileSize, tileSize);
 		mapImage = mainCanvas.snapshot(null, null);
 	}
+
+	private void replaceTileInMainCanvasToOriginal(int col, int row) {
+		mainCanvas.getGraphicsContext2D().drawImage(
+				originalMapImage,
+				col * tileSize,
+				row * tileSize,
+				tileSize, tileSize,
+				col * tileSize,
+				row * tileSize,
+				tileSize, tileSize);
+	}
+
+	private void drawCursorToMainCanvas() {
+		mainCanvas.getGraphicsContext2D().drawImage(
+				cursor.imageOption[cursor.current], 0, 0,
+				tileSize, tileSize,
+				cursor.cursorCols * tileSize,
+				cursor.cursorRows * tileSize,
+				tileSize, tileSize);
+	}
 	
+	private void updateCurrentCanvas() {
+		currentCanvas.getGraphicsContext2D().drawImage(
+				mapImage,
+				movesetCols * tileSize, movesetRows * tileSize,
+				currentNumCols * tileSize, currentNumRows * tileSize,
+				0, 0, 640, 640);
+	}
+
+
+	private void updateMoveset() {
+		if (movesetCols > cursor.cursorCols) {
+			movesetCols --;
+		}
+		else if (movesetRows > cursor.cursorRows) {
+			movesetRows --;
+		}
+		else if (movesetCols + currentNumCols - 1 < cursor.cursorCols) {
+			movesetCols ++;
+		}
+		else if (movesetRows + currentNumRows - 1 < cursor.cursorRows) {
+			movesetRows ++;
+		}
+
+	}
+
+	private void changeCursorColor() {
+		if (cursorColor == true) {
+			cursor.current = tileType[cursor.cursorRows][cursor.cursorCols];
+		}
+		else {
+			cursor.current = 2;
+		}
+	}
+	
+	public void turningOnCurorColor() {
+		cursorColor = true;
+
+		changeCursorColor();
+
+		replaceTileInMainCanvasToOriginal(cursor.cursorCols, cursor.cursorRows);
+
+		updateItemsDraw();
+		drawCursorToMainCanvas();
+		mapImage = mainCanvas.snapshot(null, null);
+		updateCurrentCanvas();
+
+	}
+
+
+	public void cursorUp() {
+		if (cursor.cursorRows > 0) {
+			replaceTileInMainCanvasToOriginal(cursor.cursorCols, cursor.cursorRows);
+
+			cursor.cursorRows --;
+			changeCursorColor();
+
+			updateItemsDraw();
+			drawCursorToMainCanvas();
+
+			mapImage = mainCanvas.snapshot(null, null);
+
+			updateMoveset();
+			updateCurrentCanvas();
+		}
+	}
+
+	public void cursorDown() {
+		if (cursor.cursorRows < numRows - 1 ) {
+			replaceTileInMainCanvasToOriginal(cursor.cursorCols, cursor.cursorRows);
+
+			cursor.cursorRows ++;
+			changeCursorColor();
+
+			updateItemsDraw();
+			drawCursorToMainCanvas();
+
+			mapImage = mainCanvas.snapshot(null, null);
+
+			updateMoveset();
+			updateCurrentCanvas();
+		}
+	}
+
+	public void cursorLeft() {
+		if (cursor.cursorCols > 0) {
+			replaceTileInMainCanvasToOriginal(cursor.cursorCols, cursor.cursorRows);
+
+			cursor.cursorCols --;
+			changeCursorColor();
+
+			updateItemsDraw();
+			drawCursorToMainCanvas();
+
+			mapImage = mainCanvas.snapshot(null, null);
+
+			updateMoveset();
+			updateCurrentCanvas();
+		}
+	}
+
+	public void cursorRight() {
+		if (cursor.cursorCols < numCols - 1 ) {
+			replaceTileInMainCanvasToOriginal(cursor.cursorCols, cursor.cursorRows);
+
+			cursor.cursorCols ++;
+			changeCursorColor();
+
+			updateItemsDraw();
+			drawCursorToMainCanvas();
+
+			mapImage = mainCanvas.snapshot(null, null);
+
+			updateMoveset();
+			updateCurrentCanvas();
+		}
+	}
+	
+	private void updateItemsDraw() {
+		if (axePut) {
+			mainCanvas.getGraphicsContext2D().drawImage(
+					items,
+					AXE  * tileSize, tileSize, tileSize, tileSize,
+					axeCol * tileSize,
+					axeRow * tileSize,
+					tileSize, tileSize);
+		}
+		if (boatPut) {
+			mainCanvas.getGraphicsContext2D().drawImage(
+					items,
+					BOAT  * tileSize, tileSize, tileSize, tileSize,
+					boatCol * tileSize,
+					boatRow * tileSize,
+					tileSize, tileSize);
+		}
+	}
+	
+	public int handleSetAxeRequest() {
+		int handleType;
+		cursorColor = false;
+		changeCursorColor();
+
+		replaceTileInMainCanvasToOriginal(cursor.cursorCols, cursor.cursorRows);
+		if (tileType[cursor.cursorRows][cursor.cursorCols] == 1) {
+			handleType = 1;
+		}
+		else {
+			if (axePut) {
+				replaceTileInMainCanvasToOriginal(axeCol, axeRow);
+				
+				tileType[axeRow][axeCol] = 0;
+				tileType[cursor.cursorRows][cursor.cursorCols] = 1;
+				
+				handleType = 2;
+			}
+			else {
+				handleType = 0;
+			}
+			
+    		axePut = true;
+	    	tileType[cursor.cursorRows][cursor.cursorCols] = 1;
+
+	    	axeRow = cursor.cursorRows;
+	    	axeCol = cursor.cursorCols;
+		}
+
+		updateItemsDraw();
+    	drawCursorToMainCanvas();
+		
+    	mapImage = mainCanvas.snapshot(null, null);
+    	updateCurrentCanvas();
+
+    	return handleType;
+	}
+	public int handleSetBoatRequest() {
+		int handleType;
+		cursorColor = false;
+		changeCursorColor();
+
+		replaceTileInMainCanvasToOriginal(cursor.cursorCols, cursor.cursorRows);
+
+		if (tileType[cursor.cursorRows][cursor.cursorCols] == 1) {
+			handleType = 1;
+		}
+		else {
+			if (boatPut) {
+				replaceTileInMainCanvasToOriginal(boatCol, boatRow);
+				
+				tileType[boatRow][boatCol] = 0;
+				tileType[cursor.cursorRows][cursor.cursorCols] = 1;
+				
+				handleType = 2;
+			}
+			else {
+				handleType = 0;
+			}
+
+    		boatPut = true;   
+	    	tileType[cursor.cursorRows][cursor.cursorCols] = 1;
+
+	    	boatRow = cursor.cursorRows;
+	    	boatCol = cursor.cursorCols;
+	    	
+		}
+		updateItemsDraw();
+		drawCursorToMainCanvas();
+    	mapImage = mainCanvas.snapshot(null, null);
+    	updateCurrentCanvas();
+    	return handleType;
+	}
 }
